e44d6d974df626c71bb72a5ffd9994e2a51ffbdf
songjein <jeinsong100@gmail.com>
2018-06-01 14:09:40 +0900

    api

--- stdio-api/api.js
+++ stdio-api/api.js
@@ -1,38 +1,54 @@
 var express = require('express');
 var app = express();
 
+// allow CORS request
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /**
  *	in-memory channels object
- *	[
- *		{ "pw": password, "todos": [] }
- *		, {...}
+ *	{	
+ *		id_1: { "pw": password, "todos": [] }
+ *		, id_2: {...}
  *		, ...	
- *	]
+ *	}	
  *	
  *	"todos" array's elements look like
- * 	{ "todo" : todo-item, "timestamp": timestamp for synchronize }
+ * 	{ "todo" : todo-item, "timestamp": timestamp for synchronization }
  * 
  *	keep the values for 1 week
  */
 var channels = {
+};
+var todos = {
 };
 
 app.get('/', function(req, res) {
 	res.json(channels);	
 });
 
+// TODO: 인증 추가? write/ read 구분
 app.get('/getch/:id/:tmpidx', function(req, res) {
 	let ret = {
 		"status": "OK",
 		"tmpidx": req.params.tmpidx // 임시로 for문과 callback에서의 idx 싱크를 위해 추가
 	}
 
-	if (channels[req.params.id]) {
-		ret["content"] = channels[req.params.id];	
+	const id = req.params.id;
+	
+	if (id in channels) {
+		if (todos[req.params.id].length > 0) {
+			ret["content"] = todos[req.params.id];	
+		}
+		else {
+			ret["status"] = "EMPTY"; 
+		}
 	}
 	else {
-		ret["status"] = "EMPTY";
+		ret["status"] = "NOCH"; 
 	}
 	res.json(ret);
 });
@@ -40,7 +56,9 @@ app.get('/getch/:id/:tmpidx', function(req, res) {
 app.get('/mkch', function(req, res) {
 	const id = req.query.id;
 	const pw = req.query.pw;
-	channels[id] = { "pw": pw, "todos": [] };
+	//channels[id] = { "pw": pw, "todos": [] };
+	channels[id] = pw; // TODO: 암호화 하기
+	todos[id] = [];
 	res.send("channel list : " + JSON.stringify(channels) );	
 });
 
@@ -51,9 +69,10 @@ app.get('/pubch', function(req, res) {
 
 	// 비번 체크 생략
 
-	channels[id]["todos"].push({todo: td, timestamp: Date.now()});
+	//channels[id]["todos"].push({todo: td, timestamp: Date.now()});
+	todos[id].push({todo: td, timestamp: Date.now()});
 
-	res.send("Publish item to ch" + JSON.stringify(channels[id]));	
+	res.send("Publish item to ch" + id + " / "  + JSON.stringify(todos[id]));	
 });
 
 app.listen(48484, function() {
