74c12b6e5d30927bf845b3a510a6d1a1cce975ed
songjein <jeinsong100@gmail.com>
2019-03-02 18:04:25 +0900

    간단히 코드 정리, 디스크립션 추가

--- cmd/settings.js
+++ cmd/settings.js
@@ -1,6 +1,7 @@
 module.exports = {
+	API: 'https://tdls.dev',
 	TODOFILE: __dirname + "/data/todos.json",
 	FINFILE: __dirname + "/data/fin.json",
-	SUBCHFILE: __dirname + "/data/subch.json"
+	SUBCHFILE: __dirname + "/data/subch.json",
 };
 
