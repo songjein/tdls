e8a8be3a713cb8b5b174570317781fa76254c6f2
songjein <jeinsong100@gmail.com>
2019-03-02 21:47:15 +0900

    settings->globals, todo-item에 생성/완료 시간 추가

--- cmd/globals.js
+++ cmd/globals.js
@@ -3,5 +3,8 @@ module.exports = {
 	TODOFILE: __dirname + "/data/todos.json",
 	FINFILE: __dirname + "/data/fin.json",
 	SUBCHFILE: __dirname + "/data/subch.json",
+	FIN_AT: 'finished_at',
+	ADD_AT: 'created_at',
+	EMPTY_MSG: "Empty!, you can add an item using 'td add'",
 };
 
